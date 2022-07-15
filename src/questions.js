66925f2db49f84872b6ca44b8548b441c9bf1f37
Max McDonough <max.mcdonough@gmail.com>
2022-07-15 00:04:47 -0400

    Added questions

--- src/questions.js
+++ src/questions.js
@@ -1,22 +1,66 @@
 const questions = [{
     type: 'input',
-    message: 'Enter Employee name',
-    name: 'name',
+    message: "Welcome to the Team Profile Generator. Please enter your Manager's name:",
+    name: 'managerName'
 },
 {
     type: 'input',
-    message: 'Enter Employee ID',
-    name: 'id',
+    message: "Manager's ID:",
+    name: 'managerId',
 },
 {
     type: 'input',
-    message: 'Enter Employee email',
-    name: 'email',
+    message: "Manager's email:",
+    name: 'managerEmail',
 },
 {
     type: 'input',
-    message: 'Enter Employee github',
-    name: 'github',
-}];
+    message: "Manager's office number:",
+    name: 'officeNumber',
+},
+{
+    type: 'confirm',
+    message: 'Would you like to enter another Employee?',
+    name: 'newEmployee',
+    choices: ['yes', 'no'],
+},
+{
+    type: 'list',
+    message: 'Which employee type would you like to add?',
+    name: 'employeeChoice',
+    choices: ['Engineer', 'Intern'],
+    when: (answers) => answers.newEmployee === true,
+},
+{
+    type: 'input',
+    message: "Please enter the employee's name:",
+    name: 'employeeName',
+    when: (answers) => answers.newEmployee === true,
+},
+{
+    type: 'input',
+    message: "Employee's ID:",
+    name: 'employeeId',
+    when: (answers) => answers.newEmployee === true,
+},
+{
+    type: 'input',
+    message: "Employee's email:",
+    name: 'employeeEmail',
+    when: (answers) => answers.newEmployee === true,
+},
+{
+    type: 'input',
+    message: "Employee's github profile:",
+    name: 'employeeGithub',
+    when: (answers) => answers.employeeChoice === 'Engineer',
+},
+{
+    type: 'input',
+    message: "Employee's school:",
+    name: 'employeeschool',
+    when: (answers) => answers.employeeChoice === 'Intern',
+}
+];
 
 export default questions;
